11daa392ae455cf9a479531b411e8c1065aed742
MOhhh <64732359+MOhhh-ok@users.noreply.github.com>
2024-06-26 12:04:33 +0900

    -

--- plugin-x/webpack.config.js
+++ plugin-x/webpack.config.js
@@ -12,6 +12,10 @@ module.exports = {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
             },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
         ],
     },
     resolve: {
